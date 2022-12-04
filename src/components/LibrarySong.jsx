b917725fe4c24b54c0678240177ec36433975516
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-12-04 12:05:29 +0600

    selected song in library

--- src/components/LibrarySong.jsx
+++ src/components/LibrarySong.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying, setSongs }) => {
     const handleSongSelect = () => {
         const selectedSong = songs.filter((state) => state.id === id);
         setCurrentSong(selectedSong[0]);
+
+        // add active state 
+        const newSongs = songs.map((song) => {
+            if (song.id === id) {
+                return {
+                    ...song, active: true,
+                }
+            } else {
+                return {
+                    ...song, active: false,
+                }
+            }
+        })
+        setSongs(newSongs)
 
         // check if the song is playing 
         if (isPlaying) {
@@ -17,7 +31,7 @@ const LibrarySong = ({ song, songs, setCurrentSong, id, audioRef, isPlaying }) =
     }
 
     return (
-        <div onClick={handleSongSelect} className='library-song'>
+        <div onClick={handleSongSelect} className={`library-song ${song.active ? 'selected' : ""}`}>
             <img src={song.cover} alt={song.name} />
             <div className="song-description">
                 <h3>{song.name}</h3>
